298e320e864823f90280ff28ea4f5baeaa1d2ea7
Raphael Marmol <raphael.marmol.vdb@gmail.com>
2025-10-02 09:57:14 +0200

    Correction navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -16,8 +16,8 @@ const Navbar = () => {
   const { logoutMutation } = useLogout();
 
   return (
-    <nav className="bg-base-200 border-b border-base-300 sticky top-0 z-30">
-      <div className="px-4 sm:px-6 lg:px-8 py-5 flex items-center">
+    <nav className="bg-base-200 sticky top-0 z-30">
+      <div className="px-4 sm:px-6 lg:px-8 py-5 border-b border-base-300 flex items-center">
         <div className="flex items-center justify-between w-full">
           {/* Logo visible uniquement en page de chat */}
           {isChatPage && (
